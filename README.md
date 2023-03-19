9f92e649a3945b07f6f509bf875cab4c299e5095
이시흔 <dudwo5600@naver.com>
2023-03-19 14:54:09 +0000

    nodejs study

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # node_first
-To study nodejs !
+To study nodejs ! ( 생활코딩 nodejs 강의 )
+
+<h2>강의 요약</h2>
+
+- url이란? (http://opentutorials.org:3000/main?id=HTML&page=12)에서 
+http는 protocol, opentutorials.org는 host(domain), 3000은 port 번호, main은 path,
+id=HTML&page=12는 query string을 의미한다.
+
+- nodejs에서 파일 읽기 (readFile을 이용한다.)
+
+```
+const fs = require('fs');
+fs.readFile('sample.txt','utf8',(err,data)=>{
+console.log(data);
+});
+```
+
--- main.js
+++ main.js
@@ -1,17 +1,68 @@
 let http = require('http');
 let fs = require('fs');
+let url = require('url');
+
 let app = http.createServer(function(request,response){
-    let url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
+    let _url = request.url;
+	let queryData = url.parse(_url, true).query;
+	let pathname=url.parse(_url,true).pathname;
+	
+	if (pathname==='/'){
+		if (queryData.id === undefined){
+			let title='Welcome';
+			let data='Hello, Node.js !';
+			const template=`
+			<!doctype html>
+				<html>
+					<head>
+						<title>WEB1 - ${title}</title>
+						<meta charset="utf-8">
+					</head>
+					<body>
+						<h1><a href="/">WEB</a></h1>
+						<ol>
+							<li><a href="/?id=HTML">HTML</a></li>
+							<li><a href="/?id=CSS">CSS</a></li>
+							<li><a href="/?id=JavaScript">JavaScript</a></li>
+						</ol>
+						<h2>${title}</h2>
+						<p>${data}</p>
+					</body>
+				</html>
+			`;
+			response.writeHead(200);
+			response.end(template);
+		}else{
+			fs.readFile(`data/${queryData.id}`,'utf8',(err,data)=>{
+			let title=queryData.id;
+			const template=`
+			<!doctype html>
+				<html>
+					<head>
+						<title>WEB1 - ${title}</title>
+						<meta charset="utf-8">
+					</head>
+					<body>
+						<h1><a href="/">WEB</a></h1>
+						<ol>
+							<li><a href="/?id=HTML">HTML</a></li>
+							<li><a href="/?id=CSS">CSS</a></li>
+							<li><a href="/?id=JavaScript">JavaScript</a></li>
+						</ol>
+						<h2>${title}</h2>
+						<p>${data}</p>
+					</body>
+				</html>
+			`;
+			response.writeHead(200);
+			response.end(template);
+	});
+		}
+	} else{
 		response.writeHead(404);
-		response.end();
-		return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+		response.end("Not found");
+	}
+	
  
 });
 app.listen(3000);

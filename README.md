7791cbfa133d13a8ae0e84dd6d43e47dcbda2300
이시흔 <dudwo5600@naver.com>
2023-03-21 14:35:12 +0000

    nodejs study

--- README.md
+++ README.md
@@ -25,3 +25,13 @@ console.log(data); // CSS, HTML, JavaScript 출력됨
 });
 ```
 
+- nodejs는 동기와 비동기 처리 방식을 가지는데, 동기는 single-cycle 느낌이면 
+비동기는 쓰레드 느낌인 듯 하다.
+
+- nodejs는 npm이라는 package manager를 이용한다.
+
+- html의 form태그에서 주의할 점 : 사용자가 데이터를 가져올 때는 method="get"을 이용하고, 
+사용자가 데이터를 수정, 삭제 등을 할 때는 method="post"를 이용
+
+- post 방식으로 은밀하게 보낸 데이터를 처리하고자 할 때는 require('querystring')을 이용하여 
+request.on(~) 을 통해 값을 얻어낼 수 있다. 
--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
+let qs = require('querystring');
 
 function templateHTML(title,list,body){
 	return `
 		<!doctype html>
 			<html>
 				<head>
-					<title>WEB1 - ${title}</title>
+					<title>WEB - ${title}</title>
 					<meta charset="utf-8">
 				</head>
 				<body>
 					<h1><a href="/">WEB</a></h1>
 					${list}
+					<a href="/create">create</a>
 					${body}
 				</body>
 			</html>
@@ -31,6 +33,9 @@ function templateList(filelist){
 	list = list + '</ul>';
 	return list;
 }
+
+// request는 요청할 때 웹브라우저가 보낸 정보들
+// response는 응답할 때 우리가 웹브라우저한테 전송할 정보들
 
 let app = http.createServer(function(request,response){
     let _url = request.url;
@@ -67,7 +72,48 @@ let app = http.createServer(function(request,response){
 			});
 		});
 	}
-	} else{
+	} 
+	else if(pathname==="/create"){
+		fs.readdir('./data',(err,filelist)=>{
+			
+				let title='create';
+				const list=templateList(filelist);
+				const template=templateHTML(title,list,
+				`
+					<form action="https://node-first-vhpbo.run.goorm.site/process_create"
+					method="post">
+						<p><input type="text" name="title" placeholder="title"></p>
+						<p><textarea name="description" placeholder="description"></textarea></p>
+						<p><input type="submit" value="Submit"></p>
+					</form>
+				`);
+				
+				response.writeHead(200);
+				response.end(template);
+		});
+	}
+	else if(pathname==="/process_create"){
+		let body='';
+		
+		/* 
+		nodejs에서 웹브라우저에 보낸 정보가 크거나해서 처리하기 힘들 때
+		서버쪽에서 조각조각낸 데이터를 수신할 때 마다 콜백함수를 호출
+		*/
+		request.on('data',function(data){
+			body +=data;
+		});
+		
+		// end에 해당되는 콜백함수가 실행됐을 때 정보수신이 끝났다.
+		request.on('end',function(data){
+			const post = qs.parse(body);
+			const title=post.title;
+			const description = post.description;
+		});
+		response.writeHead(200);
+		response.end('success');
+	}
+	
+	else{
 		response.writeHead(404);
 		response.end("Not found");
 	}

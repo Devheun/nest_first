5cdd666aa188b84a333c308ceaa19942b41cd6dc
이시흔 <dudwo5600@naver.com>
2023-04-03 14:08:10 +0000

    nestjs-study

--- nestjs-study/src/boards/boards.controller.ts
+++ nestjs-study/src/boards/boards.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
 	// 제한접근자를 이용하여 생성자안에서 써주면 
 	// 파라미터가 암묵적으로 클래스 프로퍼티로 선언됨
 	constructor(private boardsService : BoardsService){}
+	
+	@Get('/')
+	getAllBoard() : Board[]{
+		return this.boardsService.getAllBoards();
+	}
 }
--- nestjs-study/src/boards/boards.service.ts
+++ nestjs-study/src/boards/boards.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+	// 다른 컴포넌트에서 값 수정 막기위해 private
+	// 게시물이 복수일 수 있기에 배열인 Board[]로 타입 지정
+	private boards: Board[]=[];
+	
+	getAllBoards():Board[]{
+		return this.boards;
+	}
+}
